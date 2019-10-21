11bdad915b243b7da64b5bf3cbf27b65ab3ec089
Onat Saygı <info@onatsaygi.com>
2019-10-21 16:15:46 +0300

    Added linked list
    Linked list implemented with pointers

---
+++ BIL_2001_algoritmalar-ve-veri-yapilari/node.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+#ifndef NODE
+#define NODE
+#endif
+
+typedef struct Node {
+	struct Node* next;
+	int value;
+} Node;
+
+Node* newNode(Node* next, int value) {
+	Node* n = (Node*)malloc(sizeof(Node));
+	n->next = next;
+	n->value = value;
+	return n;
+}
